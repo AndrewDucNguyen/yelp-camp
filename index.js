b1a1912fa53e6009ea96d54d5562019d30d169c2
Andrew Nguyen <anguyen0615@gmail.com>
2026-02-08 07:42:23 -0600

    Created User model, installed Passport packages and configure Passport

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const ejsMate = require('ejs-mate')
-const session = require('express-session')
-const flash = require('connect-flash')
-const Joi = require('joi')
-const ExpressError = require('./utils/ExpressError')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const Joi = require('joi');
+const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgroundsRoutes = require('./routes/campgrounds');
-const reviewsRoutes = require('./routes/reviews')
+const reviewsRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"))
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected")
+    console.log("Database connected");
 })
 
 const app = express();
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
@@ -40,8 +43,14 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
 }
-app.use(session(sessionConfig))
-app.use(flash())
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+// We don't need to add user and password above because of this method
+// This adds onto the UserSchema
+UserSchema.plugin(passportLocalMongoose.default)
+
+module.exports = mongoose.model('User', UserSchema)
