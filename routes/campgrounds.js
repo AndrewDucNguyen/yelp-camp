1a1a617d6a87db085b56322564c7119de26e3b9c
Andrew Nguyen <anguyen0615@gmail.com>
2026-02-13 23:07:47 -0600

    Implementing router.route method to simply routes and group them

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,11 +6,9 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 
+/*
 // List of all campgrounds
 router.get('/', catchAsync(campgrounds.index));
-
-// Serves the form to create new campground
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 // Creates the new campground to add to the list of all campgrounds
 router.post('/', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.createCampgrounds));
@@ -18,13 +16,27 @@ router.post('/', isLoggedIn, isAuthor, validateCampground, catchAsync(campground
 // Shows one specific campground
 router.get('/:id', catchAsync(campgrounds.showCampground));
 
-// Serves the edit form
-router.get('/:id/edit', isLoggedIn, catchAsync(campgrounds.renderEditForm));
-
 // Replaces the entire campground with the specific id with the new updated one and puts it for that id
 router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds));
 
 // Deletes specific campground with associated id
 router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+*/
+
+// You can use router.route to group together simliar routes
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.createCampgrounds));
+
+// Serves the form to create new campground
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router.route('/:id')
+    .get('/:id', catchAsync(campgrounds.showCampground))
+    .put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds))
+    .delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+// Serves the edit form
+router.get('/:id/edit', isLoggedIn, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
